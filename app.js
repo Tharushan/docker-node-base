3b35d4275fd188db80bb100235a09955f81891f5
Tharushan <saseeskumar.tharushan@gmail.com>
2017-11-09 10:42:15 +0100

    Config depending on env

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-//Load express module with `require` directive
+'use strict';
+
 const express = require('express')
 const app = express()
+const mysql = require('mysql');
+const config = require('konfig')();
+
+const connection = mysql.createConnection(config.db);
+
+connection.connect();
 
 //Define request response in root URL (/)
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World !')
 })
 
+const port = process.argv[2] || 8000;
 //Launch listening server on port 8081
-app.listen(8000, function () {
-  console.log('app listening on port 8000 !')
+app.listen(port , () => {
+  console.log(`app listening on port ${port}!`)
 })
+
+connection.query('SELECT 1 + 1 AS solution',  (error, results, fields) => {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
